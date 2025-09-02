6cc5a026443a48d6f1ad0fa97394fe80e866d7a4
memymini <minhi0614@gmail.com>
2025-09-02 19:23:24 +0900

    refactor:dashboard 페이지 컴포넌트 분리

---
+++ src/components/dashboard/Badge.tsx
@@ -0,0 +1,32 @@
+import Section from "@/components/common/Section";
+import Button from "@/components/common/Button";
+import { BadgeResponse } from "@/app/(pages)/dashboard/page";
+
+export default function Badge({ data }: { data: BadgeResponse }) {
+  return (
+    <Section>
+      <div className="flex h-full flex-1 flex-col gap-5">
+        <h2 className="headline-small text-text-primary">나의 뱃지 현황</h2>
+        <div className="bg-surface-2 border-surface-3 flex h-fit justify-between rounded-xl border-1 p-7">
+          <div className="flex">
+            <p className="text-6xl">🍓</p>
+            <div>
+              <p className="label-large text-text-accent">{data.status}</p>
+              <p className="headline-small text-text-primary">
+                {data.badgeLevel}
+              </p>
+            </div>
+          </div>
+
+          <div className="flex h-full flex-col items-end justify-between">
+            <p className="label-large text-text-secondary">인증일</p>
+            <p className="title-medium text-text-primary">{data.badgeDate}</p>
+          </div>
+        </div>
+        <Button size="md" href="/my-badges">
+          내 뱃지 관리 및 고유 태그 확인하기
+        </Button>
+      </div>
+    </Section>
+  );
+}
