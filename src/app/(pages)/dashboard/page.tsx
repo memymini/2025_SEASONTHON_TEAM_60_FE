918f4b9e5c7731171904212692ff1f162e80ae01
memymini <minhi0614@gmail.com>
2025-09-02 12:13:50 +0900

    refactor:뱃지 없는 경우 UI 공통 컴포넌트로 분리

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -2,7 +2,7 @@
 import Button from "@/components/common/Button";
 import Section from "@/components/common/Section";
 import { useRouter } from "next/navigation";
-import BadgeIcon2 from "@public/assets/badge-icon2.svg";
+import NoBadge from "@/components/common/NoBadge";
 
 export default function DashboardPage() {
   const router = useRouter();
@@ -17,28 +17,7 @@ export default function DashboardPage() {
       <h1 className="headline-large">{data.name}님, 안녕하세요!</h1>
 
       {/* 뱃지가 없는 경우 UI */}
-      <Section className="justify-between">
-        <div className="flex h-full flex-1 flex-col gap-5">
-          <h2 className="headline-small text-text-primary">
-            아직 발급받은 뱃지가 없어요.
-          </h2>
-          <p className="text-text-primary body-large">
-            첫번째 뱃지를 발급받고 당신의 신뢰도를 증명해보세요.
-            <br />
-            1차 해커톤에서는 소득 증명 뱃지를 발급받을 수 있습니다.
-          </p>
-          <Button
-            size="md"
-            type="button"
-            onClick={() => router.push("/verify/upload")}
-          >
-            소득 증명 인증 시작하기
-          </Button>
-        </div>
-        <div className="bg-surface-3 flex h-full w-40 items-center justify-center">
-          <BadgeIcon2 className="h-20 w-20" />
-        </div>
-      </Section>
+      <NoBadge />
 
       {/* 뱃지가 있는 경우 UI */}
       <Section>
