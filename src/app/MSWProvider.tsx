282203a704c71193216210df6c2a31819dfda156
memymini <minhi0614@gmail.com>
2025-09-03 13:59:56 +0900

    feat:msw 설치 및 provider 생성

---
+++ src/app/MSWProvider.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface MSWProviderProps {
+  children: React.ReactNode;
+}
+
+export function MSWProvider({ children }: MSWProviderProps) {
+  const [mswReady, setMswReady] = useState(false);
+
+  useEffect(() => {
+    if (
+      process.env.NODE_ENV === "development" &&
+      typeof window !== "undefined"
+    ) {
+      import("../mocks/browser").then(({ worker }) => {
+        worker
+          .start({
+            onUnhandledRequest: "bypass",
+          })
+          .then(() => setMswReady(true));
+      });
+    } else {
+      setMswReady(true);
+    }
+  }, []);
+
+  if (process.env.NODE_ENV === "development" && !mswReady) {
+    return <div>Loading...</div>;
+  }
+
+  return <>{children}</>;
+}
