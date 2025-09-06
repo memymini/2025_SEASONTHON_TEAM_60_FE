7618d9554dc6dbd6063329a53624d7100167f221
memymini <minhi0614@gmail.com>
2025-09-06 11:59:29 +0900

    chore:대시보드 페이지 에러처리 UI 추가

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -3,16 +3,19 @@ import NoBadge from "@/components/common/NoBadge";
 import { DashboardSkeleton } from "@/components/common/Skeleton";
 import Badge from "@/components/dashboard/Badge";
 import { useDashboardQuery } from "@/hooks/useDashboard";
+import Error from "@/app/error";
 
 export default function DashboardPage() {
-  const { data, isLoading, isError } = useDashboardQuery();
+  const { data, isLoading, isError, error, refetch } = useDashboardQuery();
 
   if (isLoading) return <DashboardSkeleton />;
-  if (isError) return <div>error</div>;
+  if (isError) return <Error error={error} reset={refetch} />;
   if (data) {
     return (
       <div className="flex h-fit w-full flex-col gap-10">
-        <h1 className="headline-large">{data.username}님, 안녕하세요!</h1>
+        <h1 className="headline-large text-text-primary">
+          {data.username}님, 안녕하세요!
+        </h1>
         {data.badgeLevel ? <Badge data={data} /> : <NoBadge />}
       </div>
     );
