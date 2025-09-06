0083c5528e1908a2a0c634914b75299048257b7f
memymini <minhi0614@gmail.com>
2025-09-07 00:04:08 +0900

    chore: 카카오 콜백 로직 변경

--- src/app/auth/kakao/callback/page.tsx
+++ src/app/auth/kakao/callback/page.tsx
@@ -1,22 +1,31 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useKakaoLoginQuery } from "@/hooks/useKakaoLogin";
+import { useRouter, useSearchParams } from "next/navigation";
 import Loading from "@/components/common/Loading";
-import Error from "@/app/error";
+import { useEffect } from "react";
 
 export default function KakaoCallbackPage() {
-  const router = useRouter();
-  const { data, isPending, isError, error, refetch } = useKakaoLoginQuery();
-  if (isError) return <Error error={error} reset={refetch} />;
+  // const router = useRouter();
+  // const { data, isPending, isError, error, refetch } = useKakaoLoginQuery();
 
-  if (data) {
-    localStorage.setItem("accessToken", data.accessToken);
-    localStorage.setItem("refreshToken", data.refreshToken);
-    router.replace("/dashboard");
-    return;
-  }
-  router.replace("/");
+  // useEffect(() => {
+  //   if (data) {
+  //     localStorage.setItem("token", data.token);
+  //     router.replace("/dashboard");
+  //   }
+  // }, [data, router]);
+
+  // if (isError) return <Error error={error} reset={refetch} />;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const token = searchParams.get("token"); // URL 쿼리에서 token 추출
+    if (token) {
+      localStorage.setItem("token", token);
+      router.replace("/dashboard"); // 토큰 저장 후 대시보드로 이동
+    }
+  }, [searchParams, router]);
 
   return (
     <div className="fixed inset-0 z-[9999]">
@@ -24,11 +33,12 @@ export default function KakaoCallbackPage() {
       <div className="grid h-full place-items-center">
         <div className="border-surface-3 rounded-md border bg-white/80 px-6 py-4 shadow backdrop-blur">
           <p className="body-medium">
-            {isPending
+            로그인 처리 중...
+            {/* {isPending
               ? "카카오 로그인 처리 중…"
               : isError
                 ? "로그인 실패, 이동 중…"
-                : "완료, 이동 중…"}
+                : "완료, 이동 중…"} */}
           </p>
         </div>
       </div>
